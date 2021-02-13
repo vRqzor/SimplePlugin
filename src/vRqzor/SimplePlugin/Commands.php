7a0a7b636a590499863222893d80853b728d1ed4
vRqzor <78993357+vRqzor@users.noreply.github.com>
2021-02-13 15:40:08 +1100

    Create Commands.php

---
+++ src/vRqzor/SimplePlugin/Commands.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace vRqzor\SimplePlugin;
+
+# Uses
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+
+# Commands
+class Commands extends PluginBase
