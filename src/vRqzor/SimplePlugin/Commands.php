50dddaf8f98a7e2f99da3c066c317e00e8a9ba30
vRqzor <78993357+vRqzor@users.noreply.github.com>
2021-02-13 15:45:18 +1100

    Update Commands.php

--- src/vRqzor/SimplePlugin/Commands.php
+++ src/vRqzor/SimplePlugin/Commands.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_type=1);
+
 namespace vRqzor\SimplePlugin;
 
 # Uses
@@ -8,4 +10,12 @@ use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 
 # Commands
-class Commands extends PluginBase
+class Commands extends PluginBase{
+  public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{4
+    switch($command->getName()){
+        class "hello":
+        $sender->sendMessage("Hello, " . $sender->$getName() . "!");
+        return true;
+        } 
+    }
+}                                                                                                 
